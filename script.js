6178795c722037a22872c26701cd950d738df461
SahilGaikwad2049 <gaikwadsahil1211@gmail.com>
2024-07-08 23:24:39 +0530

    backspace added

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log("code")
 
-const pattern = /[a-z]/
+const pattern = /[a-z]/;
 let currentGuessCount = 1;
 let currentGuess = document.querySelector('#guess' + currentGuessCount)
 // let currentLetters = currentGuess.dataset.letters;
@@ -11,14 +11,40 @@ document.addEventListener("keydown", (e) => {
     let press = e.key;
     if(press.length == 1 && pattern.test(press)){
         console.log('isLetter');
-        updateLetters(press)
+        updateLetters(press.toUpperCase())
+    } else if(e.key === 'Backspace'){
+        deleteLetters()
     }
-})
+});
 
-const updateLetters = (letter) => {
-    console.log('currentGuessCount = ' + currentGuessCount)
-    console.log('currentLetters = ' + currentGuess.dataset.letters)
-    currentGuess.dataset.letters = currentGuess.dataset.letters + letter;
-    currentGuess.innerHTML = currentGuess.dataset.letters
-    console.log('updated currentLetters: ' + currentGuess.dataset.letters)
+function updateLetters(letter){
+    let oldLetters = currentGuess.dataset.letters;
+    let newLetters = oldLetters + letter;
+    let currentTile = newLetters.length;
+    currentGuess.dataset.letters = newLetters;
+    // console.log('currentTile = '+ currentTile)
+    // console.log('currentGuessCount = ' + currentGuessCount)
+    // console.log('currentLetters = ' + currentGuess.dataset.letters)
+    // console.log('updated currentLetters: ' + currentGuess.dataset.letters)
+
+    updateTiles(currentTile,letter);
+}
+
+function updateTiles(tileNumber, letter){
+    // console.log('updateTiles('+ tileNumber + letter +')')
+    let currentTile = document.querySelector('#tile'+ tileNumber)
+    currentTile.innerText = letter;
+}
+
+function deleteLetters(){
+    let oldLetters = currentGuess.dataset.letters;
+    console.log(oldLetters)
+    let newLetters = oldLetters.slice(0, -1)
+    console.log(newLetters)
+    currentGuess.dataset.letters = currentGuess.dataset.letters.slice(0,-1);
+    deleteTiles(oldLetters.length)
+}
+
+function deleteTiles(tileNumber){
+    document.querySelector('#tile'+ tileNumber).innerText = "";
 }
